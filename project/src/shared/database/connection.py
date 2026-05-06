b6d89d0b85c5ad34d2704be28b49eff7837ad1e4
Lix-Roses <albgal2215@outlook.com>
2026-05-06 18:53:37 -0500

    Feat (migraciones y correeciones)
    Se implementaron migraciones al proyecto, ademas de crear los archivos requirements necesarios con las dependencias.

--- project/src/shared/database/connection.py
+++ project/src/shared/database/connection.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from shared.config.settings import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def create_tables():
-    from shared.base.base_entity import Base
-    import modules.user.domain.user_entity  # esto registra la tabla
-    Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
